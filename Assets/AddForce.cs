85ab6d3bc0c1fd7b8d3039570d47ffd30556e0a6
estebanmont <esmon.ben@gmail.com>
2022-08-12 09:57:11 -0500

    VR controllables

---
+++ Assets/AddForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    [SerializeField] Vector3 force;
+    [SerializeField]GameObject mySelf;
+    Rigidbody myBody;
+    private void Start()
+    {
+        myBody = mySelf.GetComponent<Rigidbody>(); 
+    }
+   public void shoot()
+    {
+        myBody.AddForce(force);
+    }
+}
